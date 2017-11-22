cd55c1d3bf73ea5ed869e19f5ac421ee30809f2a
wangdahoo <157195705@qq.com>
2017-11-22 23:50:51 +0800

    add cookie and localStorage helper

--- src/services/index.js
+++ src/services/index.js
@@ -1,3 +1,8 @@
 import Vue from 'vue'
 import http from './http'
+import cookie from './cookie'
+import Storage from 'storage-js-iso'
+
 window.$http = Vue.prototype.$http = http
+window.$cookie = Vue.prototype.$cookie = cookie
+window.$storage = Vue.prototype.$storage = Storage
